1b45ec4766264cd5098da9ea14e01ad96a7d509a
도넛맨 <hp196@naver.com>
2023-02-16 19:43:30 +0900

    회원가입 테스트 코드

---
+++ src/test/java/sns/controller/UserControllerTest.java
@@ -0,0 +1,53 @@
+package sns.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import sns.controller.request.UserJoinRequest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void 회원가입() throws Exception {
+        String userName = "hello";
+        String password = "1111";
+
+        mockMvc.perform(post("/api/v1/users/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+        ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void 회원가입시_이미회원가입된_userName으로_회원가입을_하는경우() throws Exception {
+        String userName = "hello";
+        String password = "1111";
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+                ).andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+
+
+
+}
