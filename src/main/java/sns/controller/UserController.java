fa2e208dfb967a979117da2af71d245c3f21ccde
도넛맨 <hp196@naver.com>
2023-02-18 21:47:30 +0900

    service 추가

---
+++ src/main/java/sns/controller/UserController.java
@@ -0,0 +1,20 @@
+package sns.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sns.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public void join() {
+        userService.join();
+    }
+}
